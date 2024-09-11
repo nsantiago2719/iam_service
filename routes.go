cc98d944e1707c789e7621ee0ef712bf65a6a056
norman santiago <nsantiago@ethernalabs.com>
2024-09-12 06:49:54 +0800

    add logout routes

--- routes.go
+++ routes.go
@@ -9,6 +9,7 @@ import (
 func IamRoutes(r *mux.Router) {
 	userPrefix := r.PathPrefix("/users").Subrouter()
 	r.HandleFunc("/auth", Auth).Methods(http.MethodPost)
+	r.HandleFunc("/logout", Logout).Methods(http.MethodDelete)
 
 	userPrefix.HandleFunc("/{id}", UserDetails).Methods(http.MethodGet)
 }
