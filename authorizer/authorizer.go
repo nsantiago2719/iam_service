86ece7448ef021870b06f7ce09337f7ebb0c235a
norman santiago <nsantiago@ethernalabs.com>
2024-09-16 20:54:14 +0800

    update authorizer package
    rename ExtractClaim to GetValidClaim
    added new func called isAuthorzied

--- authorizer/authorizer.go
+++ authorizer/authorizer.go
@@ -1,6 +1,7 @@
 package authorizer
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
@@ -10,25 +11,31 @@ import (
 )
 
 // Role used for creating struct for role data from database
-type role struct {
+type Role struct {
 	Name        string         `json:"name"`
 	Permissions types.JSONText `json:"permissions"`
 }
 
+type Permission struct {
+	Access   string   `json:"access"`
+	Action   string   `json:"action"`
+	Resource []string `json:"resource"`
+}
+
 // payload contains the payload for the jwt token
-type payload struct {
-	roles []*role `json:"roles"`
+type Payload struct {
+	Roles []*Role `json:"roles"`
 }
 
 type Claims struct {
-	Data payload `json:"data"`
+	Data Payload `json:"data"`
 	jwt.RegisteredClaims
 }
 
 var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
-// ExtractClaim returns the jwt claim and bool for token validitiy
-func ExtractClaim(token string) (*Claims, error) {
+// GetValidClaim returns the jwt claim and bool for token validitiy
+func GetValidClaim(token string) (*Claims, error) {
 	tokenVal, err := jwt.ParseWithClaims(token,
 		&Claims{},
 		func(_ *jwt.Token) (interface{}, error) {
@@ -39,19 +46,22 @@ func ExtractClaim(token string) (*Claims, error) {
 	}
 
 	if claims, ok := tokenVal.Claims.(*Claims); ok && tokenVal.Valid {
-		return claims, nil
+		if auth := isAuthorized(claims); auth == true {
+			return claims, nil
+		}
 	}
 
 	// by default return invalid token
 	return nil, errors.New("Invalid token")
 }
 
-func AuthorizedAccess(resource, action, token string) error {
-	_, err := ExtractClaim(token)
-	if err != nil {
-		fmt.Println(err)
+func isAuthorized(claims *Claims) bool {
+	permissions := []Permission{}
+	for _, r := range claims.Data.Roles {
+		json.Unmarshal(r.Permissions, &permissions)
 	}
 
-	// roles := []role{}
-	return nil
+	fmt.Println(&permissions)
+
+	return true
 }
