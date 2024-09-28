45eb42c7a932d24f2d0dad25b9ceda101bb5eae5
norman santiago <nsantiago@ethernalabs.com>
2024-09-28 09:43:50 +0800

    remove routes and added it to api

--- api.go
+++ api.go
@@ -41,3 +41,11 @@ func (s *API) Create() {
 	log.Println("iam service running on port", s.listenAddr)
 	http.ListenAndServe(s.listenAddr, router)
 }
+
+func (s *API) IamRoutes(r *mux.Router) {
+	userPrefix := r.PathPrefix("/users").Subrouter()
+	r.HandleFunc("/auth", s.Auth).Methods(http.MethodPost)
+	r.HandleFunc("/logout", s.Logout).Methods(http.MethodDelete)
+
+	userPrefix.HandleFunc("/{id}", UserDetails).Methods(http.MethodGet)
+}
